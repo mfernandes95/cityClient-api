116655f2697647307eef36c98efb227d0ac6c11e
mpinheiro <mpinheiro@orion.tech>
2020-10-04 17:03:20 -0300

    ignore swagger on test

--- jest.config.js
+++ jest.config.js
@@ -18,7 +18,11 @@ module.exports = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ["src/**", "!src/database/schemas/**"],
+  collectCoverageFrom: [
+    "src/**",
+    "!src/database/schemas/**",
+    "!src/swagger.json",
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "__tests__/coverage",
