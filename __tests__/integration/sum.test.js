516f44bbf074539017af11e379e61aac000151dd
mpinheiro <mpinheiro@orion.tech>
2020-10-04 15:27:55 -0300

    config jest with mongodb and synthax import

--- __tests__/integration/sum.test.js
+++ __tests__/integration/sum.test.js
@@ -1,9 +1,19 @@
+import mongoose from "mongoose";
 import City from "../../src/app/schemas/City";
-// import City from "../../src/app/schemas/City";
-// const { City } = require("../../src/app/schemas");
-// const { City } = require("../../src/app/schemas");
 
 describe("Sum", () => {
+  beforeAll(async () => {
+    if (!process.env.MONGO_URL)
+      throw new Error("MongoDB server not initialized");
+
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useFindAndModify: true,
+      useCreateIndex: true,
+    });
+  });
+
   it("sum two numbers", async () => {
     const x = 4;
     const y = 2;
